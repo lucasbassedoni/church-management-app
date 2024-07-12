c5eca18a12be192c5265748145b670435e75e96b
Lucas Bassedoni <lucashbassedoni@gmail.com>
2024-07-12 12:32:42 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 axios.defaults.withCredentials = true;
 
 const api = axios.create({
-    baseURL: 'http://church-management-api.vercel.app', //backend URL
+    baseURL: 'https://church-management-api-production.up.railway.app/', //backend URL
 });
 
 export default api;
